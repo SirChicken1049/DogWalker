5f92a2d6f7726b8b243d3f7c2b921336e538a8c5
EvanBurman <evanb77@nycstudents.net>
2025-09-17 11:50:00 -0400

    DogWalker Start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany c = new DogWalkCompany();
+        System.out.println(c);
+        int hour = c.addDogs();
+        System.out.println(c);
+        DogWalker w = new DogWalker(4, c);
+        public int walkDogs(int hour) {
+            int dogs = company.numAvailableDogs(hour);
+        }
+    }
+}
